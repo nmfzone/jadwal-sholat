f933dfc6d336b99cb2ff6c8f082722bc77f8b724
nmfzone <nabilftd@gmail.com>
2016-06-08 01:18:12 +0700

    Fixed indentation

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ if (process.argv.length <= 2) {
 
 var param = process.argv[2]
 if (param == '--version' || param == '-v') {
-	print(VERSION.green)
-	exit()
+  print(VERSION.green)
+  exit()
 }
 else if (!param.match(/^[a-zA-Z\s]+$/i)) {
   print('\nInvalid city name!'.yellow)
   displayFooter()
-	exit()
+  exit()
 }
 
 // Check is city available
@@ -37,48 +37,48 @@ spinner.setSpinnerString(7)
 spinner.start()
 
 var horseman = new Horseman()
-	horseman
-	.userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/50.0.2661.102 Chrome/50.0.2661.102 Safari/537.36")
-	.on('error', function(message, trace) {
-		print(message)
-	})
-	.on('timeout', function(message) {
-		print('Timeout\n')
-	})
-	.open('http://jadwalsholat.pkpu.or.id/monthly.php?id='+cityId)
-	.evaluate(function() {
-		return document.querySelector('.table_adzan tbody').innerHTML
-	})
-	.then(function(result) {
-		jadwalsholat.load(result).parse()
-	})
-	.finally(function() {
-	    horseman.close()
-	    spinner.stop()
-	    return
-	})
+  horseman
+  .userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/50.0.2661.102 Chrome/50.0.2661.102 Safari/537.36")
+  .on('error', function(message, trace) {
+    print(message)
+  })
+  .on('timeout', function(message) {
+    print('Timeout\n')
+  })
+  .open('http://jadwalsholat.pkpu.or.id/monthly.php?id='+cityId)
+  .evaluate(function() {
+    return document.querySelector('.table_adzan tbody').innerHTML
+  })
+  .then(function(result) {
+    jadwalsholat.load(result).parse()
+  })
+  .finally(function() {
+    horseman.close()
+    spinner.stop()
+    return
+  })
 
 var jadwalsholat = {
-	result: null,
+  result: null,
   tables: null,
-	checkResult() {
-		return (this.result != '' ? true:false)
-	},
-	load(res) {
-		this.parseHTML(res)
-		this.result = res
-		return this
-	},
-	parseHTML(html) {
-		var table = cheerio.load(html)
+  checkResult() {
+    return (this.result != '' ? true:false)
+  },
+  load(res) {
+    this.parseHTML(res)
+    this.result = res
+    return this
+  },
+  parseHTML(html) {
+    var table = cheerio.load(html)
     this.tables = table('tr.table_header').nextAll()
-	},
+  },
   getThisDayTimeScheduleTable() {
     let header = ["Shubuh", "Terbit", "Dzuhur", "Ashar", "Maghrib", "Isya"]
     let body = []
     var tbl = new Table(),
-        date = new Date().getDate(),
-        table = this.tables[date-1].children
+    date = new Date().getDate(),
+    table = this.tables[date-1].children
 
     let row = []
     for (let i=1; i<table.length; i++) {
@@ -93,16 +93,16 @@ var jadwalsholat = {
 
     print(thisDayTable.toString())
   },
-	parse() {
-		if (this.checkResult()) {
+  parse() {
+    if (this.checkResult()) {
       print("\n\n")
       this.getThisDayTimeScheduleTable()
-		}
-		else {
-			print("\nSorry, we can't process your request right now :(".yellow)
-		}
-	  displayFooter()
-	}
+    }
+    else {
+      print("\nSorry, we can't process your request right now :(".yellow)
+    }
+    displayFooter()
+  }
 }
 
 function checkCity(name) {
@@ -120,7 +120,7 @@ function displayFooter() {
 }
 
 function print(msg = '') {
-	console.log(msg)
+  console.log(msg)
 }
 
 function exit() {
